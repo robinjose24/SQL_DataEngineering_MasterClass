f5eb4358537760d97ef57c0bedb0a8e9fba51126
ROBIN JOSE R <160265144+robinjose24@users.noreply.github.com>
2025-12-26 13:42:26 +0530

    Update 7.types_of_create_table.sql

--- 7.types_of_create_table.sql
+++ 7.types_of_create_table.sql
@@ -1,3 +1,4 @@
+✅ Done 
 My complete SQL Masterclass Notes in Tanglish is now live! Covers 30+ topics with syntax, examples, and step-by-step explanations. 
 Perfect for both beginners and professionals to master SQL easily. Check it out here 👉 https://topmate.io/dataengineering/1697791
 
