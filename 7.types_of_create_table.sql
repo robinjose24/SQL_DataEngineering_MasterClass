fe3cea48235407e3297026d408def7468046fdb9
ROBIN JOSE R <160265144+robinjose24@users.noreply.github.com>
2025-12-27 12:43:11 +0530

    Modify status message in SQL notes
    Updated the status message for SQL Masterclass notes.

--- 7.types_of_create_table.sql
+++ 7.types_of_create_table.sql
@@ -1,4 +1,4 @@
-✅ Done 
+ ✅ Done 
 My complete SQL Masterclass Notes in Tanglish is now live! Covers 30+ topics with syntax, examples, and step-by-step explanations. 
 Perfect for both beginners and professionals to master SQL easily. Check it out here 👉 https://topmate.io/dataengineering/1697791
 
